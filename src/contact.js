69d86a06fc221606c4a73c1525d9f3fe58d29339
Carlee Powell <crpowell@live.unc.edu>
2020-05-28 11:49:03 -0400

    changed copy on contact

--- src/contact.js
+++ src/contact.js
@@ -8,7 +8,7 @@ class Contact extends Component {
         <h1 className="title">CONTACT</h1>
         <p><a href="mailto:crpowell924@gmail.com" className="has-text-link">crpowell924@gmail.com</a></p>
         <p>(919)454-8306</p>
-        <p>So you like what you saw, huh? For all things business, the best way to reach me is at my 
+        <p>Did you like what you saw? For all things business, the best way to reach me is at my 
         <a href="mailto:crpowell924@gmail.com" className="has-text-link"> email. </a></p>
         <p>You can also find me on social media, linked in the icons at the bottom of the page.</p>   
         <p>Or in case you like things a little more formal, here's a pdf of my <a href={resume} className="has-text-link">resume</a>. </p>
